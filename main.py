07eb21e381f8e25fc9adea79e36d51f5e3f415fd
hazellnut <tom.g.a.hazell@gmail.com>
2024-03-13 16:37:05 +1100

    cleaning

--- main.py
+++ main.py
@@ -48,21 +48,16 @@ AdsPortCloseProto = ctypes.WINFUNCTYPE(
     ctypes.c_void_p
 )
 
-
-
 AdsPortOpen = AdsPortOpenProto(("AdsPortOpenEx",tc_dll),None)
 nAddr = Addr()
 pAddr = ctypes.pointer(nAddr)
 
 AdsPortClose = AdsPortCloseProto(("AdsPortCloseEx",tc_dll),None)
 portno = AdsPortOpen(None)
-# AdsGetLocalAddress = tc_dll.AdsGetLocalAddressEx
-# err = AdsGetLocalAddress(portno,pAddr)
 
 netId = NetId()
 ams_addr = [192,168,20,48,2,1]
-# for i,_ in enumerate(netId.b):
-#     netId.b[i] = ams_addr[i]
+
 
 netId.b = (ctypes.c_ubyte *6)(*ams_addr)
 nAddr = Addr()
@@ -70,43 +65,23 @@ nAddr.netId = netId
 nAddr.port = 65535
 adsportno = portno
 pAddr = ctypes.pointer(nAddr)
-# AdsSyncReadStateParams = (1,"port",0),(1,"pAddr",0),(1,"pAdsState",0),(1,"pDeviceState",0)
 
-
-
-# char_data = bytearray([192,168,20,48,1,1])
-# L = len(char_data)
-# netId.b = (ctypes.c_ubyte * L)  (*char_data)
-# nAddr.AmsNetId = netId
-# nAddr.port = ctypes.c_uint16(801)
-
-# AdsSyncReadStateReq = AdsSyncReadStateReqProto(("AdsSyncReadStateReqEx",tc_dll),AdsSyncReadStateParams)
 AdsSyncReadStateReq = tc_dll.AdsSyncReadStateReqEx
 
 AdsState = ctypes.c_uint16()
 DeviceState = ctypes.c_uint16()
 pAdsState = ctypes.pointer(AdsState)
 pDeviceState = ctypes.pointer(DeviceState)
-# AddrPointer = ctypes.pointer(nAddr)
-
-# pDeviceStatePtr = (ctypes.Pointer(ctypes.c_short))
-# pDeviceStatePtr = ctypes.byref(pDeviceState)
 
 errorcode = AdsSyncReadStateReq(portno,pAddr,pAdsState,pDeviceState)
-
-
-
 
 AdsGetDllVersionProto = ctypes.WINFUNCTYPE(SAdsVersion,ctypes.c_void_p)
 dllversionfunc = AdsGetDllVersionProto(("AdsGetDllVersion",tc_dll),None)
 
-
-
-
 returnval = dllversionfunc(None)
 
-
 AdsPortClose(None)
+
 if errorcode:
     print(f"error: {errorcode}")
 else:
